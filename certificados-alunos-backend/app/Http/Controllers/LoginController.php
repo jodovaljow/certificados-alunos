56a22ab1a2f9df609f658c4cf2de28de260e0755
Jodoval Luiz dos Santos Junior <jowrhcp@hotmail.com>
2021-10-23 21:57:41 -0300

    refactor: (back) route login

--- certificados-alunos-backend/app/Http/Controllers/LoginController.php
+++ certificados-alunos-backend/app/Http/Controllers/LoginController.php
@@ -5,15 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
     public function login(Request $request)
     {
-        $credentials = $request->validate([
+        $validator = Validator::make($request->all(), [
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
+
+        if ($validator->fails()) {
+
+            return Response('', 403);
+        }
+
+        $credentials = $validator->validate();
 
         if (Auth::attempt($credentials)) {
 
