863b0428229a5bb0db5fe25d99b6fde1441ae333
Jodoval Luiz dos Santos Junior <jowrhcp@hotmail.com>
2021-10-29 19:19:25 -0300

    feat: adjust top menu

--- certificados-alunos-frontend/src/app/topo/topo.component.ts
+++ certificados-alunos-frontend/src/app/topo/topo.component.ts
@@ -1,4 +1,7 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Observable, of, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topo',
@@ -11,9 +14,48 @@ export class TopoComponent implements OnInit {
 
   @Output() onLogout = new EventEmitter()
 
-  constructor() { }
+  destroyed = new Subject<void>()
+  currentScreenSize$: Observable<string> = of('')
+  displayNameMap = new Map([
+    [Breakpoints.XSmall, 'XSmall'],
+    [Breakpoints.Small, 'Small'],
+    [Breakpoints.Medium, 'Medium'],
+    [Breakpoints.Large, 'Large'],
+    [Breakpoints.XLarge, 'XLarge'],
+  ]);
+
+  constructor(breakpointObserver: BreakpointObserver) {
+
+    this.currentScreenSize$ = breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ])
+      .pipe(
+        takeUntil(this.destroyed),
+        map(
+          result => {
+
+            for (const query of Object.keys(result.breakpoints)) {
+              if (result.breakpoints[query]) {
+                return this.displayNameMap.get(query) ?? 'Unknown';
+              }
+            }
+
+            return 'Unknown'
+          }
+        )
+      )
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
   }
 
   logout() {
